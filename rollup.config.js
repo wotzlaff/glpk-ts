2db4a27e60a8614709a6731255693b09a616b41e
Nico Strasdat <Nico.Strasdat@tu-dresden.de>
2021-04-14 13:04:59 +0200

    Use rollup to bundle

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import typescript from 'rollup-plugin-typescript2'
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/index.js',
+    format: 'umd',
+    name: 'GLPK',
+    globals: {
+      'glpk-wasm': 'glpkWasm',
+    },
+  },
+  external: ['glpk-wasm'],
+  plugins: [typescript(), resolve(), commonjs()],
+}
